0f6a85d6ce14ea16ce4af3885016a6c15782f67c
Luis Sandoval <luismsandoval96@gmail.com>
2022-01-30 20:56:21 -0800

    added css header and footer animation and html button animated by javascript

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ if(time <= 11){
 }
 
 document.write('Hi, ' + name + '! ' + message + ' ' + bird + ' is my favorite type of bird too!');
+
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 910) {
+            clearInterval(id);
+        } else {
+            pos++;
+            /* elem.style.top = pos + 'px'; */
+            elem.style.left = pos + 'px';
+        }
+    }
+}
